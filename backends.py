590125b8e90b8d0369df2f64f19c7bd9c481ac17
Modou Sawo <msawo.inc@gmail.com>
2017-01-14 23:42:12 -0500

    Update backends.py

--- backends.py
+++ backends.py
@@ -2,7 +2,7 @@ from registration.backends.simple.views import RegistrationView
 
 # new reg view, subclassing RegistrationView from the plugin
 class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user=None): # removed request, note when I changed panels to users, now accepts request, weird!
+    def get_success_url(self, request, user=None): 
         # the named URL we want to redirect to after
         # successful Registration
         return ('registration_create_my_solar')
