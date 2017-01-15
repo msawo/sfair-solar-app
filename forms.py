b115dd9572e87e9db0277ef227356787c79d59aa
Modou Sawo <msawo.inc@gmail.com>
2017-01-14 23:39:28 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,7 @@
+from django import forms
+
+from . import models
+
 from django.forms import ModelForm
 from collection.models import Profile
 
@@ -6,3 +10,16 @@ class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ('name', 'address',)
+
+
+class InstallationForm(forms.ModelForm):
+    class Meta:
+        model = models.Installation
+        fields = [
+            'title',
+            'order',
+            'time_lapsed',
+            'description',
+        ]
+
+# config this class later
