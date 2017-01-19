956fc2602c455efed980f4213234e334fc93fc1f
Modou Sawo <msawo.inc@gmail.com>
2017-01-18 22:17:03 -0500

    Update models.py

--- models.py
+++ models.py
@@ -6,3 +6,14 @@ class Profile(models.Model):
     address = models.TextField()
     slug = models.SlugField(unique=True)
     user = models.OneToOneField(User, blank=True, null=True)
+
+
+# Just adding this new class to be used for later 
+class Charge(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.charField(max_length=255)
+    percentage = models.PercentField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
