6c78176eb768949f72506b1d7e2c6e17073d9ac5
Modou Sawo <msawo.inc@gmail.com>
2017-01-26 08:05:01 -0500

    Update backends.py

--- backends.py
+++ backends.py
@@ -3,6 +3,4 @@ from registration.backends.simple.views import RegistrationView
 # new reg view, subclassing RegistrationView from the plugin
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user=None): 
-        # the named URL we want to redirect to after
-        # successful Registration
         return ('registration_create_my_solar')
