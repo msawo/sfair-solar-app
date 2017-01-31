fd0011b820218bf5794cabfaaff1af2883bfcc18
msawo <msawo.inc@gmail.com>
2017-01-30 22:40:58 -0500

    Updated the structural isses
    Good to go!

--- backends.py
+++
@@ -1,6 +0,0 @@
-from registration.backends.simple.views import RegistrationView
-
-# new reg view, subclassing RegistrationView from the plugin
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user=None): 
-        return ('registration_create_my_solar')
